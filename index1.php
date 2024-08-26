36e8b10f16dccdeabf454728824b3d43bd7e8f84
Juan Jose Romero Montes <claseinfojuanjose@gmail.com>
2024-08-26 11:28:50 +0200

    Feature: index 1

---
+++ index1.php
@@ -0,0 +1,12 @@
+<?php
+
+$rooms = [
+    ['id' => 1, 'name' => 'Suite', 'number' => '101', 'price' => 1500, 'discount' => 10],
+    ['id' => 2, 'name' => 'Double bedroom', 'number' => '102', 'price' => 1000, 'discount' => 30],
+    ['id' => 3, 'name' => 'Single bedroom', 'number' => '103', 'price' => 500, 'discount' => 50]
+]
+?>
+
+<pre>
+    <?php print_r($rooms) ?>
+</pre>
